35b8c11a39985ea8e01cb64f36087ea597aa6913
Arkadiusz Lis <alis@ARTE206>
2019-01-22 16:59:38 +0100

    start configuration project

---
+++ src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PizzaImage from "../components/PizzaImage/PizzaImage";
+
+class Users extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Pizza Image</h1>
+                <p> Awesome users on boards of this course!</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
